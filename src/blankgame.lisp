bf2c293a0997419986279131c0fe1b4b5c7658c6
Alexandr Popolitov <popolit@gmail.com>
2015-11-05 11:23:11 +0100

    Actually make the screen of the blank game blank

--- src/blankgame.lisp
+++ src/blankgame.lisp
@@ -5,9 +5,13 @@
   "Just open a window with a title and some handlers bound"
   (with-init (:everything)
     (with-window (win :title "Hello CL-PYGAMES World!" :flags '(:shown))
-      (with-event-loop (:method :poll)
-	(:keyup
-	 (:keysym keysym)
-	 (when (sdl2:scancode= (sdl2:scancode-value keysym) :scancode-escape)
-	   (sdl2:push-event :quit)))
-	(:quit () t)))))
+      (with-renderer (rend win)
+	(set-render-draw-color rend 0 0 0 255)
+	(render-clear rend)
+	(render-present rend)
+	(with-event-loop (:method :poll)
+	  (:keyup
+	   (:keysym keysym)
+	   (when (sdl2:scancode= (sdl2:scancode-value keysym) :scancode-escape)
+	     (sdl2:push-event :quit)))
+	  (:quit () t))))))
